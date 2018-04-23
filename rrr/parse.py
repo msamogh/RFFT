d33bb426ab9bb460e26be825f5047038cf53bb4e
root <msamogh@gmail.com>
2018-04-23 11:27:35 +0000

    Init text parsing function

--- rrr/parse.py
+++ rrr/parse.py
@@ -1,9 +1,11 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
 from xml.etree import ElementTree as elem
 
+import os
 import autograd.numpy as np
 
 
-def get_mask(bbox_path, image_size, valid_class_names=[]):
+def get_image_mask(bbox_path, image_size, valid_class_names=[]):
     masked_img = np.zeros(image_size, dtype="uint8")
 
     root = elem.parse(bbox_path).getroot()
@@ -21,3 +23,11 @@ def get_mask(bbox_path, image_size, valid_class_names=[]):
             get_coord('xmin'):get_coord('xmax')
         ] = 1
     return masked_img
+
+
+def get_text_mask(text_files_base_dir, annotations):
+    for file in annotations:
+        text = open(os.path.join(text_files_base_dir, file), 'r').read()
+        for a in annotations:
+            start, end = a.split(',')
+            
