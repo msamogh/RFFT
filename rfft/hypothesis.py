8a584679f28a16b6685d50d2e09a65754cbc81f1
root <msamogh@gmail.com>
2018-04-26 17:37:47 +0000

    Incremental hypothesis adding

--- rfft/hypothesis.py
+++ rfft/hypothesis.py
@@ -11,5 +11,11 @@ class Hypothesis(object):
         self.normalize = normalize
 
 
+    @staticmethod
+    def incrementally_sample(annotations, hypothesis_load_fn, mask_shape, increment=5):
+        for i in range(0, len(annotations), increment):
+            yield hypothesis_load_fn(mask_shape, annotations[:i + 1])
+
+
     def __repr__(self):
-    	return 'Weight: %f' % self.weight
+        return 'Hypothesis: (weight = %f)' % self.weight
