6144789dbdccd111f4cc3315dd80ace537ece915
Amogh <msamogh@gmail.com>
2018-04-27 10:37:54 +0530

    WTF

--- examples/image/parse.py
+++ examples/image/parse.py
@@ -7,7 +7,7 @@ import autograd.numpy as np
 
 
 def get_image_mask(bbox_path, image_size, valid_class_names=[]):
-    masked_img = np.zeros(image_size, dtype='uint8')
+    masked_img = np.ones(image_size, dtype='uint8')
 
     root = elem.parse(bbox_path).getroot()
     for obj in root.findall('object'):
@@ -22,7 +22,7 @@ def get_image_mask(bbox_path, image_size, valid_class_names=[]):
         masked_img[
             get_coord('ymin'):get_coord('ymax'),
             get_coord('xmin'):get_coord('xmax')
-        ] = 1
+        ] = 0
     return masked_img
 
 
