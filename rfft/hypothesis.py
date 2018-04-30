c1035e1945a1d9882f131824d18842a4661f56d9
root <msamogh@gmail.com>
2018-04-30 11:47:58 +0000

    Specify weight per annotation

--- rfft/hypothesis.py
+++ rfft/hypothesis.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 
 class Hypothesis(object):
@@ -7,11 +8,17 @@ class Hypothesis(object):
         self,
         A,
         weight=10,
+        per_annotation=True,
         normalize=False
     ):
         self.A = A
-        self.weight = weight
+        if per_annotation:
+            num_annotations = len(np.where(A != 0)[0])
+            self.weight = weight * num_annotations
+        else:
+            self.weight = weight
         self.normalize = normalize
+        self.per_annotation = per_annotation
 
     @staticmethod
     def incrementally_sample(annotations, hypothesis_load_fn, mask_shape, increment=5, shuffle=True):
