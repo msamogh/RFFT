589c6e64d3ed8f0a11d253dd540dceb9b46f383b
root <msamogh@gmail.com>
2018-04-30 13:49:17 +0000

    Refactoring

--- examples/image/parse.py
+++ examples/image/parse.py
@@ -10,15 +10,13 @@ def get_image_mask(bbox_path, image_size, valid_class_names=[]):
     masked_img = np.ones(image_size, dtype='uint8')
 
     root = elem.parse(bbox_path).getroot()
-    for obj in root.findall('object'):
-        if valid_class_names:
-            if obj.find('name').text not in valid_class_names:
-                continue
+    annotations = root.findall('object')
+    if valid_class_names:
+        annotations = filter(lambda x: x.find('name').text in valid_class_names, annotations)
+
+    for obj in annotations:
         bbox = obj.find('bndbox')
-
-        def get_coord(name):
-            return int(bbox.find(name).text)
-
+        get_coord = lambda name: int(bbox.find(name).text)
         masked_img[
             get_coord('ymin'):get_coord('ymax'),
             get_coord('xmin'):get_coord('xmax')
