add71960d89a1737c92976faebf35758609ae035
root <msamogh@gmail.com>
2018-05-01 16:26:55 +0000

    Reformat code

--- rfft/hypothesis.py
+++ rfft/hypothesis.py
@@ -4,8 +4,7 @@ import numpy as np
 
 class Hypothesis(object):
     """Class representing a hypothesis for a dataset."""
-
-
+    
     def __init__(self,
                  A,
                  weight=10,
@@ -13,8 +12,12 @@ class Hypothesis(object):
         """Initialize a hypothesis.
 
         Args:
-            A: A numpy array representing the binary mask matrix. The array is of shape (Number of samples x Length of each sample vector).
-            weight: Weight of the "right reasons" term in the objective function. The weight can either be specified as a constant for the entire dataset or on a "per annotation" basis. In the latter case, the weight is multiplied with the number of non-zero masks to arrive at the final weight.
+            A: A numpy array representing the binary mask matrix. The array is of shape (Number
+            of samples x Length of each sample vector).
+            weight: Weight of the "right reasons" term in the objective function. The weight can
+            either be specified as a constant for the entire dataset or on a "per annotation"
+            basis. In the latter case, the weight is multiplied with the number of non-zero masks
+            to arrive at the final weight.
             per_annotation: True if weight specified is "per annotation".
         """
         self.A = A
@@ -24,17 +27,16 @@ class Hypothesis(object):
         else:
             self.weight = weight
         self.per_annotation = per_annotation
-
-
+    
     def __repr__(self):
         """String representation of hypothesis metadata."""
         if self.per_annotation:
-            return 'Hypothesis: weight={}, per_annotation={}, num_annotations={}'.format(self.weight, 
-                                                                                         self.per_annotation,
-                                                                                         self.num_annotations)
+            return 'Hypothesis: weight={}, per_annotation={}, num_annotations={}'.format(
+                self.weight,
+                self.per_annotation,
+                self.num_annotations)
         return 'Hypothesis: weight={}, per_annotation={}'.format(self.weight, self.per_annotation)
-
-
+    
     @staticmethod
     def incrementally_sample(annotations,
                              hypothesis_load_fn,
@@ -42,16 +44,19 @@ class Hypothesis(object):
                              increment=5,
                              shuffle=True,
                              **hypothesis_params):
-    """Return an iterator of hypotheses in which more annotations are sampled in each iteration.
+        
+        """Return an iterator of hypotheses in which more annotations are sampled in each iteration.
+
+        Args:
+            annotations: List of paths of all annotations.
+            hypothesis_load_fn: A function that takes in the mask shape, list of annotations and
+            hypothesis params, and returns a Hypothesis object.
+            mask_shape: Mask shape passed to hypothesis_load_fn.
+            increment: Number of annotations added in each iteration.
+            shuffle: If True, shuffles the list of annotations before sampling.
+        """
     
-    Args:
-        annotations: List of paths of all annotations.
-        hypothesis_load_fn: A function that takes in the mask shape, list of annotations and hypothesis params, and returns a Hypothesis object.
-        mask_shape: Mask shape passed to hypothesis_load_fn.
-        increment: Number of annotations added in each iteration.
-        shuffle: If True, shuffles the list of annotations before sampling.
-    """
-        if shuffle:
-            random.shuffle(annotations)
-        for i in range(0, len(annotations) + 1, increment):
-            yield hypothesis_load_fn(mask_shape, annotations[:i], **hypothesis_params)
+    if shuffle:
+        random.shuffle(annotations)
+    for i in range(0, len(annotations) + 1, increment):
+        yield hypothesis_load_fn(mask_shape, annotations[:i], **hypothesis_params)
--- rfft/multilayer_perceptron.py
+++ rfft/multilayer_perceptron.py
@@ -9,11 +9,6 @@ from autograd.misc.optimizers import adam
 
 from .perceptron import Perceptron, one_hot
 from .hypothesis import Hypothesis
-# from local_linear_explanation import LocalLinearExplanation
-
-# Adapted from https://github.com/HIPS/autograd/blob/master/examples/neural_net.py
-# with modifications made such that we have a first-class MLP object
-# and such that our loss function includes an explanation penalty.
 
 
 def relu(inputs):
@@ -36,20 +31,24 @@ def feed_forward(params, inputs, nonlinearity=relu):
 def input_gradients(params, y=None, scale='log'):
     # log probabilities or probabilities
     if scale is 'log':
-        def p(x): return feed_forward(params, x)
+        def p(x):
+            return feed_forward(params, x)
     else:
-        def p(x): return np.exp(feed_forward(params, x))
-
+        def p(x):
+            return np.exp(feed_forward(params, x))
+    
     # max, sum, or individual y
     if y is None:
         y = 'sum' if scale is 'log' else 'max'
     if y is 'sum':
         p2 = p
     elif y is 'max':
-        def p2(x): return np.max(p(x), axis=1)
+        def p2(x):
+            return np.max(p(x), axis=1)
     else:
-        def p2(x): return p(x)[:, y]
-
+        def p2(x):
+            return p(x)[:, y]
+    
     return elementwise_grad(p2)
 
 
@@ -58,8 +57,8 @@ def l2_irrelevant_input_gradients(params, X, A, **kwargs):
 
 
 def init_random_params(scale, layer_sizes, rs=npr):
-    return [(scale * rs.randn(m, n),   # weight matrix
-             scale * rs.randn(n))      # bias vector
+    return [(scale * rs.randn(m, n),  # weight matrix
+             scale * rs.randn(n))  # bias vector
             for m, n in zip(layer_sizes[:-1], layer_sizes[1:])]
 
 
@@ -77,75 +76,74 @@ def update_progress_bar(i, num_iters):
 
 
 class MultilayerPerceptron(Perceptron):
-
+    
     @classmethod
     def from_params(klass, params):
         mlp = klass()
         mlp.params = params
         return mlp
-
+    
     def __init__(
-        self,
-        layers=(50, 30),
-        l2_params=0.0001,
-        l2_grads=0.0001,
-        input_preprocessor=None
+            self,
+            layers=(50, 30),
+            l2_params=0.0001,
+            l2_grads=0.0001,
+            input_preprocessor=None
     ):
         self.l2_params = l2_params
         self.l2_grads = l2_grads
         self.layers = list(layers)
         self.input_preprocessor = input_preprocessor
-
+    
     def predict_proba(self, inputs):
         if self.input_preprocessor:
             inputs = self.input_preprocessor(inputs)
         return np.exp(feed_forward(self.params, inputs))
-
+    
     def predict(self, inputs):
         return np.argmax(feed_forward(self.params, inputs), axis=1)
-
+    
     def score(self, inputs, targets):
         return np.mean(self.predict(inputs) == targets)
-
+    
     def input_gradients(self, X, **kwargs):
         if 'scale' not in kwargs:
             kwargs['scale'] = None  # default to non-log probs
         return input_gradients(self.params, **kwargs)(X.astype(np.float32))
-
+    
     def fit(
-        self,
-        inputs,
-        targets,
-        hypothesis=None,
-        normalize=False,
-        num_epochs=64,
-        batch_size=256,
-        step_size=0.001,
-        rs=npr,
-        always_include=None,
-        nonlinearity=relu,
-        verbose=False,
-        callback=None,
-        show_progress_every=100,
-        **input_grad_kwargs
+            self,
+            inputs,
+            targets,
+            hypothesis=None,
+            normalize=False,
+            num_epochs=64,
+            batch_size=256,
+            step_size=0.001,
+            rs=npr,
+            always_include=None,
+            nonlinearity=relu,
+            verbose=False,
+            callback=None,
+            show_progress_every=100,
+            **input_grad_kwargs
     ):
         X = inputs.astype(np.float32)
         y = one_hot(targets)
         params = init_random_params(
             0.1, [X.shape[1]] + self.layers + [y.shape[1]], rs=rs)
-
+        
         batch_size = min(batch_size, X.shape[0])
         num_batches = int(np.ceil(X.shape[0] / batch_size))
         input_grads = input_gradients(
             params,
             **input_grad_kwargs
         )(inputs)
-
-
+        
         def objective(params, iteration):
             if show_progress_every is not None and iteration % show_progress_every == 0:
                 update_progress_bar(iteration, num_epochs * num_batches)
-
+            
             idx = batch_indices(iteration, num_batches, batch_size)
             Xi = X[idx]
             yi = y[idx]
@@ -154,32 +152,32 @@ class MultilayerPerceptron(Perceptron):
             else:
                 A = np.zeros_like(inputs).astype(bool)
             Ai = A[idx]
-
+            
             if always_include is not None:
                 Ai = np.vstack((A[always_include], Ai))
                 Xi = np.vstack((X[always_include], Xi))
                 yi = np.vstack((y[always_include], yi))
-
+            
             if normalize:
                 lenX = max(1., float(len(Xi)))
             else:
                 lenX = 1.
-
+            
             crossentropy = - \
-                np.sum(feed_forward(params, Xi, nonlinearity) * yi) / lenX
+                               np.sum(feed_forward(params, Xi, nonlinearity) * yi) / lenX
             if hypothesis is not None:
                 rightreasons = hypothesis.weight * \
-                    l2_norm(input_gradients(
-                        params, **input_grad_kwargs)(Xi)[Ai])
+                               l2_norm(input_gradients(
+                                   params, **input_grad_kwargs)(Xi)[Ai])
             else:
                 rightreasons = 0
             smallparams = self.l2_params * l2_norm(params)
-
+            
             if iteration % show_progress_every == 0 and verbose:
                 sys.stdout.write('Iteration={}, crossentropy={}, rightreasons={}'.format(
                     iteration, crossentropy._value, rightreasons._value))
                 sys.stdout.flush()
             return crossentropy + rightreasons + smallparams
-
+        
         self.params = adam(grad(objective), params,
                            step_size=step_size, num_iters=num_epochs * num_batches)
