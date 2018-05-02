2409827f68f303bd0cf17a2150f897da268d6cf3
root <msamogh@gmail.com>
2018-05-02 08:03:13 +0000

    Moved Flask files

---
+++ api/image.py
@@ -0,0 +1,10 @@
+from flask_restful import Resource
+
+
+class Image(Resource):
+
+	def __init__(self):
+		pass
+
+
+	def 
---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_restful import Api
+
+from api.heartbeat import HeartBeat
+from api.annotaion import Annotation
+
+
+app = Flask(__name__)
+api = Api(app)
+
+PATH_V1_API = '/api/v1'
+
+def register_endpoints(api):
+    api.add_resource(Annotation, PATH_V1_API + '/<experiment>/<sample_idx>')
+    api.add_resource(Train, PATH_V1_API + '/<experiment>')
+    api.add_resource()
+
+register_endpoints(api)
+
+app.run(host='0.0.0.0', port=8000, debug=True)
