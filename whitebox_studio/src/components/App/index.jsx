f0113857fa57983e4cf90c6912fcaa636992b89c
Shoaib <ahamedshoaibk@gmail.com>
2018-05-02 23:16:24 +0530

    basic navigation flow

--- whitebox_studio/src/components/App/index.jsx
+++ whitebox_studio/src/components/App/index.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import NavigationBar from '../NavigationBar';
 import MaskingCanvas from '../MaskingCanvas';
+import Home from '../Home';
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: 'home'
+      page: 'home',
     };
   }
 
   onNavigationClick = (page) => {
-    this.setState({page});
+    this.setState({ page });
   }
 
   renderBody = () => {
     switch (this.state.page) {
-      case 'home': return (<div>home</div>);
+      case 'home': return (<Home navigateToMaskingCanvas={() => this.onNavigationClick('imageMasker')} />);
       case 'textAnotator': return (<div>textAnotator</div>);
-      case 'imageMasker': return (<MaskingCanvas/>);
+      case 'imageMasker': return (<MaskingCanvas />);
       default: return (<div>home</div>);
     }
   }
@@ -27,7 +28,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <NavigationBar onClick={this.onNavigationClick}/>
+        <NavigationBar onClick={this.onNavigationClick} />
         <div className="App-body-card">
           {this.renderBody()}
         </div>
