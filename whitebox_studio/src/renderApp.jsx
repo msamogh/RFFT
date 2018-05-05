f7a2610e0ec50d778e218050e610997f9666bfd7
root <msamogh@gmail.com>
2018-05-05 13:06:46 +0000

    Removed unnecessary packages

--- whitebox_studio/src/renderApp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
-const renderApp = () => {
-  ReactDOM.render(<App />,document.getElementById('root'));
-};
-
-export default renderApp;
