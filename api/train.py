2b1ae70aa156cc6ae0ba4f2764567ca0de0b89f9
Amogh <msamogh@gmail.com>
2018-05-06 20:15:17 +0530

    Annotations working

--- api/train.py
+++ api/train.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from flask_restful import request, Resource
 from experiment.experiment_cache import ExperimentCache
 
@@ -7,14 +8,13 @@ import json
 class Train(Resource):
 
     def post(self, experiment_name):
-        print(experiment_name)
         req_json = json.loads(request.data)
-        print(req_json)
         try:
             num_epochs = int(req_json['num_epochs'])
             experiment = ExperimentCache().get_experiment(experiment_name)
             experiment.train(num_epochs=num_epochs)
-        except ValueError as ve:
+            return jsonify({'success': True}), 200
+        except ValueError:
             return 'num_epochs should be an integer.', 400
         except KeyError as ke:
             return str(ke), 400
